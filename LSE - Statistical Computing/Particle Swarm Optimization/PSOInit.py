16ff0b999a33f9158c1981b5a5cb0a1a773119e9
Anya P. Conti <anyapriya@yahoo.com>
2019-07-01 13:10:19 -0400

    Updating files

--- LSE - Statistical Computing/Particle Swarm Optimization/PSOInit.py
+++ LSE - Statistical Computing/Particle Swarm Optimization/PSOInit.py
@@ -1,31 +1,138 @@
 import numpy as np
 
 
-def posit_init(bound, s):
-    return np.random.uniform(bound[0], bound[1], s)
-
-
-def veloc_init(bound, s):
-    prange = bound[1] - bound[0]
-    return np.random.uniform(-abs(prange), abs(prange), s)
 
 
 
-def pso_init(f, s, bounds):
-    n = len(bounds)
 
-    # particle current positions and velocities
-    pcurr = list(map(posit_init, bounds, [s] * n))
+
+
+############################ Contents ############################
+
+# This contains the two initialization functions that are called by PSOMain.py
+# This also contains two initialization functions specifically for velocity and 
+#    position which are called by the two main initialization functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+############################ Variable descriptions ############################
+
+# D is the number of dimensions (int)
+# N is the number of particles (int)
+# bounds of the search area - of the form [[x1min, x1max], ... , [xDmin, xDmax]]
+# bound is the search area bound for one dimension (one entry of bounds)
+# f is the function to be optimized
+
+# pcurr is the current position of each particles
+# vcurr is the current velocity of each particles
+# pbest is the best position of each particle
+# fbest is the minimum value found for each particle
+# fgbest is the overall minimum value found of all particles
+# pgbest is the overall best position of each particle
+
+
+
+
+
+
+
+
+
+
+
+
+############################ Initialization Functions ############################
+
+
+
+
+# Takes in bound for one dimension, and the number of particles
+# Randomly generations the position in that dimension of each particle based on 
+#    a uniform distribution within the bounds for the dimension
+
+def posit_init(bound, N):
+    return np.random.uniform(bound[0], bound[1], N)
+
+
+
+
+
+
+
+
+# Takes in bound for one dimension, and the number of particles
+# Randomly generations the velocity in that dimension of each particle based on 
+# 	a uniform distribution between the range times -1 and the range itself
+
+def veloc_init(bound, N):
+    dimrange = abs(bound[1] - bound[0])
+    return np.random.uniform(-dimrange, dimrange, N)
+
+
+
+
+
+
+
+
+# Called by PSOMain.py
+# Takes in the function, the number of particles, and the bounds
+# Uses these to initialize all necessary starting values for PSO
+
+def pso_init(f, N, bounds):
+    D = len(bounds)
+
+    # initializes current particle positions and velocities using functions above
+    pcurr = list(map(posit_init, bounds, [N] * D))
     pcurr = np.array(list(map(list, zip(*pcurr))))
-    vcurr = list(map(veloc_init, bounds, [s] * n))
+    vcurr = list(map(veloc_init, bounds, [N] * D))
     vcurr = np.array(list(map(list, zip(*vcurr))))
 
-    # particle best position and value
+    # best particle position and value automatically starts the same as current
     pbest = np.copy(pcurr, order="k")
     fbest = np.array(list(map(f, pbest)))
 
-    # global best position and valueabs
+    # global best position and value are calculated
     fgbest = min(fbest)
     pgbest = np.copy(pbest[fbest == fgbest], order="k")[0]
 
     return pcurr, vcurr, pbest, fbest, pgbest, fgbest
+
+
+
+
+
+
+
+
+# Called by PSOMain.py
+# Takes in the function, the number of particles, and the bounds
+# Uses these to initialize all necessary starting values for QPSO
+
+def qpso_init(f, N, bounds):
+    D = len(bounds)
+
+    # initializes current particle positions using function above
+    pcurr = list(map(posit_init, bounds, [N] * D))
+    pcurr = np.array(list(map(list, zip(*pcurr))))
+
+    # best particle position and value automatically starts the same as current
+    pbest = np.copy(pcurr, order="k")
+    fbest = np.array(list(map(f, pbest)))
+
+    # global best position and value are calculated
+    fgbest = min(fbest)
+    pgbest = np.copy(pbest[fbest == fgbest], order="k")[0]
+
+    return pcurr, pbest, fbest, pgbest, fgbest
--- LSE - Statistical Computing/Particle Swarm Optimization/PSOUpdate.py
+++ LSE - Statistical Computing/Particle Swarm Optimization/PSOUpdate.py
@@ -3,29 +3,96 @@ import numpy as np
 
 
 
+
+
+
+
+############################ Contents ############################
+
+# This contains the three update functions that are called by PSOMain.py
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+############################ Variable descriptions ############################
+
+# D is the number of dimensions (int)
+# N is the number of particles (int)
+# bounds of the search area - of the form [[x1min, x1max], ... , [xnmin, xnmax]]
+# bound is the search area bound for one dimension (one entry of bounds)
+# f is the function to be optimized
+# params are the necessary parameters (omega, c1, c2) for PSO
+
+# pcurr is the current position of each particles
+# vcurr is the current velocity of each particles (PSO and parallelized PSO only)
+# pbest is the best position of each particle
+# fbest is the minimum value found for each particle
+# fgbest is the overall minimum value found of all particles
+# pgbest is the overall best position of each particle
+# mbest is mean of each particle's best position by dimension (QPSO and QPSO_par only)
+# x is the proposed movement, which is made if it is an improvement from the particles last location
+    # (QPSO and QPSO_par only)
+
+# newp is a temporary calculation of the new position, saved to pcurr if inside the bounds
+# newx_id is a temporary calculation of the new x, saved to x if inside the bounds
+
+# rpg
+# phi
+# u
+# beta
+# changeParam
+# coinToss - 50% chance of being True / False, used in QPSO to decide to add or subtract changeParam
+
+
+
+
+
+
+############################ Update Functions ############################
+
+
+
+# Called by PSOMain.py (pso_algo())
+# Takes in pcurr, vcurr, pbest for one dimension of one particle, as well as pgbest and params
+# Updates the velocity based on the PSO algorithm
+
 def veloc_update(pcurr, vcurr, pbest, pgbest, params):
     rpg = np.random.uniform(0, 1, 2)
     vcurr = params[0] * vcurr + params[1] * rpg[0] * (pbest - pcurr) + params[2] * rpg[1] * (pgbest - pcurr)
     return vcurr
 
 
-def posit_update(pcurr, vcurr, bounds):
-    newp = pcurr + vcurr
-    for i in range(len(newp)):
-        if newp[i] > bounds[0] and newp[i] < bounds[1]:
-            pcurr[i] = newp[i]
-    return pcurr
 
 
+
+
+
+
+
+# Called by PSOMain.py (pso_algo_par())
+# Takes in pcurr, vcurr, pbest, fbest for one particle, as well as pgbest, params, bounds, f
+# Performs the PSO algorithm to update pcurr, vcurr, pbest, and fbest
 
 def point_update(pcurr, vcurr, pbest, fbest, pgbest, params, bounds, f):
-    n = len(pcurr)
-    for d in range(n):
+    D = len(pcurr)
+    for d in range(D):
         vcurr[d] = veloc_update(pcurr[d], vcurr[d], pbest[d], pgbest[d], params)
     newp = pcurr + vcurr
-    for i in range(len(newp)):
-        if newp[i] > bounds[i][0] and newp[i] < bounds[i][1]:
-            pcurr[i] = newp[i] + 0
+    for d in range(D):
+        if newp[d] > bounds[d][0] and newp[d] < bounds[d][1]:
+            #Adding 0 creates a new object in memory instead of variable that references same object
+            pcurr[d] = newp[d] + 0
     fcurr = f(pcurr)
     if fcurr < fbest:
         fbest = fcurr + 0
@@ -34,6 +101,34 @@ def point_update(pcurr, vcurr, pbest, fbest, pgbest, params, bounds, f):
 
 
 
-def dist(v1, v2):
-    distt = np.sum(np.sqrt(np.sum(np.square(v1 - v2), axis=1)))
-    return distt
+
+
+
+
+
+
+
+
+
+
+# Called by PSOMain.py (qpso_algo_par())
+# Takes in x, pcurr, pbest, fbest, for one particle, as well as mbest, pgbest, beta, bounds, f
+# Performs the QPSO algorithm to update x, pcurr, pbest, and fbest
+
+def qpoint_update(x, pcurr, pbest, fbest, mbest, pgbest, beta, bounds, f):
+    D = len(x)
+    for d in range(D):
+        phi = np.random.uniform()
+        u = np.random.uniform()
+        coinToss = np.random.uniform() < 0.5
+        pcurr[d] = phi * pbest[d] + (1 - phi) * pgbest[d]
+        changeParam = beta * abs(mbest[d] - x[d]) * (-1) * np.log(u)
+        newx_id = pcurr[d] + changeParam if coinToss else pcurr[d] - changeParam
+        if newx_id > bounds[d][0] and newx_id < bounds[d][1]:
+            #Adding 0 creates a new object in memory instead of variable that references same object
+            x[d] = newx_id + 0
+    fcurr = f(x)
+    if fcurr < fbest:
+        fbest = fcurr + 0
+        pbest = x + 0
+    return x.tolist(), pcurr.tolist(), pbest.tolist(), fbest
